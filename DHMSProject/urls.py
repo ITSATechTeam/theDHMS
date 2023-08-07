961551b00a7bcf7f7e815d8857c89be26ad4adc3
Franklin <praisefranklin005@gmail.com>
2023-08-07 10:39:33 +0100

    updates

--- DHMSProject/urls.py
+++ DHMSProject/urls.py
@@ -21,9 +21,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls'))
     # 
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 if settings.DEBUG:
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
