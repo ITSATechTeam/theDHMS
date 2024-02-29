9a34eaa368bcb823937bb098d7b29c21b9983431
Franklin Isaac <praisefranklin005@gmail.com>
2024-02-29 12:17:47 +0100

    udpated staff page empty state

--- static/staff/js/main.js
+++ static/staff/js/main.js
@@ -1,9 +1,15 @@
 const registerstaffbox = document.querySelector('.regsiteruser');
 const addstaffbtn = document.querySelector('.addstaffbtn button');
+const staffemptystatesectionbtn = document.querySelector('.staffemptystatesectionbtn button');
 const closereguser = document.querySelector('.closereguser');
 const shadow1 = document.querySelector('.shadow1');
 
 addstaffbtn.addEventListener('click', () => {
+	registerstaffbox.style.display = 'block';
+	shadow1.style.display = 'block';
+})
+
+staffemptystatesectionbtn.addEventListener('click', () => {
 	registerstaffbox.style.display = 'block';
 	shadow1.style.display = 'block';
 })
